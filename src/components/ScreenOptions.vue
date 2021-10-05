b31cade3f0f040c2fdffa9367fbe5fe7c504772e
devcjlozano <carlosjlp@gmail.com>
2021-10-05 16:48:03 +0200

    add styles choices in screenOptions

--- src/components/ScreenOptions.vue
+++ src/components/ScreenOptions.vue
@@ -1,12 +1,28 @@
 <template>
   <div class="screen-options">
     <div class="screen-options__line" />
+    <div class="screen-options__choices">
+      <div class="screen-options__choices__title">
+        <span> ¿Qué animal se esconde? </span>
+      </div>
+      <ul class="screen-options__choices__ul">
+        <li><span v-text="toUpperCase('León')" /></li>
+        <li><span v-text="toUpperCase('Vaca')" /></li>
+        <li><span v-text="toUpperCase('Elefante')" /></li>
+        <li><span v-text="toUpperCase('Ciervo')" /></li>
+      </ul>
+    </div>
   </div>
 </template>
 
 <script>
 export default {
   name: "ScreenOptions",
+  methods: {
+    toUpperCase(text) {
+      return text.toUpperCase();
+    },
+  },
 };
 </script>
 
@@ -29,6 +45,23 @@ export default {
   height: 20px;
   width: calc(100% - 20px);
   background: var(--secundary);
+}
+.screen-options__choices {
+  margin-left: 5px;
+}
+.screen-options__choices__title {
+  margin: 10px 0;
+}
+.screen-options__choices__ul {
+  display: flex;
+  flex-wrap: wrap;
+  list-style: none;
+  padding-left: 5px;
+}
+.screen-options__choices__ul li {
+  flex-basis: 50%;
+  padding-left: 1rem;
+  margin-bottom: 10px;
 }
 
 @media (min-width: 992px) {
